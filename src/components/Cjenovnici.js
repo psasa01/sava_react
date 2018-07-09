d69e416f0c5d0660b33cd778dc6ada79fa1026b0
eminasale <psasa01@gmail.com>
2018-07-09 22:51:51 +0200

    row absolute

--- src/components/Cjenovnici.js
+++ src/components/Cjenovnici.js
@@ -2,22 +2,24 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom';
 
 export default class Cjenovnici extends Component {
-    
+
     render() {
         return (
-            <div className="row" style={{ width: '90%' }}>
-            <br />
-            <br />
-            <div style={{top: '0', bottom: '0', right: '0', left: '0'}}>
-                <h1>Cjenovnici</h1>
-                <Link to="/">Back</Link>
-                <h1>Cjenovnici</h1>
-                <h1>Cjenovnici</h1>
-                <h1>Cjenovnici</h1>
-                <h1>Cjenovnici</h1>
-                                                               
+
+            <div style={{ position: 'absolute', left: '5%' }} >
+                <br />
+                <br />
+                <div className="row" style={{ width: '90%' }} >
+                    <h1>Cjenovnici</h1>
+                    <Link to="/">Back</Link>
+                    <h1>Cjenovnici</h1>
+                    <h1>Cjenovnici</h1>
+                    <h1>Cjenovnici</h1>
+                    <h1>Cjenovnici</h1>
+
+                </div>
             </div>
-        </div>
+
         )
     }
 }
